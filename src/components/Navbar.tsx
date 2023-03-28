7434c0fab7353ff0c975f62b7b7076bc0bce79bf
Nkaka23dev <nkakaeric96@gmail.com>
2023-03-28 14:46:02 +0200

    feat: done with authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,33 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { auth } from '../config/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth)
+  const logUserOut = () => {
+    signOut(auth);
+    navigate('/')
+  }
   return (
     <section className='shadow-xl'>
       <div className='max-w-6xl mx-auto py-4 flex justify-between  items-center'>
         <div className='uppercase text-2xl font-bold'>
-          LOgo
+          LOGO
         </div>
-        <div className='flex gap-10 text-xl font-semibold'>
+        <div className='flex gap-10 text-xl items-center font-semibold'>
           <NavLink to="/login" className='hover:underline'>Login</NavLink>
           <NavLink to="/" className='hover:underline'>Home</NavLink>
+          <div className='flex items-center gap-6'>
+            <p className='text-blue-500 text-[1.2rem] font-semibold'>{user?.displayName}</p>
+            {user && (
+              <>
+                <button onClick={logUserOut} className='bg-red-500 hover:bg-red-600 text-white p-1 rounded-sm'>Logout</button>
+                <img className='rounded-full w-[3rem]' src={user?.photoURL || ""} alt="" />
+              </>
+            )}
+          </div>
         </div>
       </div>
     </section>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../config/firebase"
+
 
 export default function Login() {
+  const navigate = useNavigate();
+
+
+  const handleSignIn = async () => {
+    const result = await signInWithPopup(auth, provider);
+    navigate("/")
+  }
   return (
-    <div className='text-center'>Login</div>
+    <section>
+      <div className='max-w-6xl mx-auto grid w-full space-y-8'>
+        <p className='text-sky-500 text-2xl  text-center font-semibold'>Sign In With Google To Continue</p>
+        <button onClick={handleSignIn}
+          className='p-3 mx-auto px-10 rounded-md hover:bg-blue-600 bg-blue-500 text-white font-semibold'>Sign In with Google</button>
+      </div>
+    </section>
   )
 }
